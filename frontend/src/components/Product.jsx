d2bab25a442400f7e9c97f46055aabecacc899d1
Santosh Kumar Das <santoshdas.mits@gmail.com>
2024-07-11 12:51:26 +0530

    Added Routing and added linkContainer for react bootstrap

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,20 +1,23 @@
-/** @format */
+/* eslint-disable react/prop-types */
 
+
+/** @format */
+import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
 
 const Product = ({ product }) => {
-//   console.log(product);
+  //   console.log(product);
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as={"div"}>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as={"h3"}>$ {product.price}</Card.Text>
       </Card.Body>
     </Card>
