8f3535c22e9f7acdc20acbdfcb8eaa8ef4666d4f
Santosh Kumar Das <santoshdas.mits@gmail.com>
2024-07-18 16:20:35 +0530

    Set up Proxy and Fetch products from the backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,17 +1,22 @@
 /** @format */
-
+import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-import Product from "../products";
-import products from "../products";
+import axios from "axios";
 import Rating from "../components/Rating";
 const ProductScreen = () => {
+  const [product, setProduct] = useState([]);
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
-  //   console.log(product);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
   return (
     <>
-     <Link to='/' className='btn btn-light my-3'>
+      <Link to="/" className="btn btn-light my-3">
         Go Back
       </Link>
       <Row>
@@ -19,7 +24,7 @@ const ProductScreen = () => {
           <Image src={product.image} alt={product.name} fluid />
         </Col>
         <Col md={4}>
-          <ListGroup variant='flush'>
+          <ListGroup variant="flush">
             <ListGroup.Item>
               <h3>{product.name}</h3>
             </ListGroup.Item>
@@ -35,7 +40,7 @@ const ProductScreen = () => {
         </Col>
         <Col md={3}>
           <Card>
-            <ListGroup variant='flush'>
+            <ListGroup variant="flush">
               <ListGroup.Item>
                 <Row>
                   <Col>Price:</Col>
@@ -49,15 +54,15 @@ const ProductScreen = () => {
                 <Row>
                   <Col>Status:</Col>
                   <Col>
-                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                    {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
                   </Col>
                 </Row>
               </ListGroup.Item>
 
               <ListGroup.Item>
                 <Button
-                  className='btn-block'
-                  type='button'
+                  className="btn-block"
+                  type="button"
                   disabled={product.countInStock === 0}
                 >
                   Add To Cart
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @format */
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // proxy requests prefixed '/api'
+    proxy: {
+      "/api": "http://localhost:5000",
+    },
+  },
+});
